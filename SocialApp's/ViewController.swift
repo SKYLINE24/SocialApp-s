41dddaf8cff34dd8094176da931db8599d81b4da
Erbil Can Keleş <cankls2434@gmail.com>
2022-12-07 18:15:23 +0300

    giriş ekranında giriş yap ve kayıt ol için gerekli kodları yazdım firebase ile bağlantı kurdum

--- SocialApp's/ViewController.swift
+++ SocialApp's/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import Firebase
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var sifreTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    func hataMesaji(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    @IBAction func girisYapildi(_ sender: Any) {
+        if emailTextField.text != "" && sifreTextField.text != ""{
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: sifreTextField.text!){ (authdataresult, error) in
+                if error != nil{
+                    self.hataMesaji(titleInput: "Hata!", messageInput: error?.localizedDescription ?? "Hata aldınız. Tekrar deneyiniz")
+                }else{
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }
+    }
+    @IBAction func kayitOlTiklandi(_ sender: Any) {
+        if emailTextField.text != "" && sifreTextField.text != "" {
+            
+            //kullanıcı kayıt işlemleri
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: sifreTextField.text!) { authdataresult, error in
+                if error != nil{
+                    self.hataMesaji(titleInput: "Hata", messageInput: error!.localizedDescription)
+                }else{
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }else{
+            hataMesaji(titleInput: "Hata", messageInput: "Email ve şifre giriniz")
+        }
+    }
+    
+    
+   
+    
 
 }
 
