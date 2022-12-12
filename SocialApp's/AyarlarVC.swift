dd27deed7482edfd41d7fbfe0d5c04499dfad67a
Erbil Can Keleş <cankls2434@gmail.com>
2022-12-12 18:51:56 +0300

    Ayar

---
+++ SocialApp's/AyarlarVC.swift
@@ -0,0 +1,19 @@
+//
+//  AyarlarVC.swift
+//  SocialApp's
+//
+//  Created by Erbil Can on 12.12.2022.
+//
+
+import UIKit
+
+class AyarlarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
