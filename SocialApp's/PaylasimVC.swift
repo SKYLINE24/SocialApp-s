1a34d5baf6a7e1a7a3cc6843c7a78a88e385144d
Erbil Can Keleş <cankls2434@gmail.com>
2022-12-15 16:49:04 +0300

    PaylasimVC de değişiklerde bulundum eksik kaldı düzeltilmesi gereken ve eklenmesi gereken şeyler var hata da o yüzden veriyor

--- SocialApp's/PaylasimVC.swift
+++ SocialApp's/PaylasimVC.swift
@@ -6,13 +6,51 @@
 //
 
 import UIKit
+import Firebase
+import SDWebImage
 
-class PaylasimVC: UIViewController {
+class PaylasimVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var postDizisi = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        firebaseVerileriAl()
 
+    }
+    func firebaseVerileriAl(){
+        let firestoreDatabase = Firestore.firestore()//database değişkenimizi oluşturduk
+        
+        firestoreDatabase.collection("Post").order(by: "tarih", descending: true)
+            .addSnapshotListener { (snapshot, error) in
+            
+            if error != nil{
+                print(error?.localizedDescription)
+            }else{
+                if snapshot?.isEmpty != true && snapshot != nil{
+                    //her feed sayfasına geçtiğinde üstüne ekleyecek olduğu için silip aşşağıda tekrar yüklenmesini sağlıyoruz
+                    self.postDizisi.removeAll(keepingCapacity: false)
+                    for document in snapshot!.documents{
+                        if let gorselUrl = document.get("gorselUrl") as? String{
+                            if let yorum = document.get("yorum") as? String{
+                                if let email = document.get("email") as? String{
+                                    let post = Post(email: email, yorum: yorum, gorselUrl: gorselUrl)
+                                    self.postDizisi.append(post)
+                                }
+                            }
+                        }
+                    }
+                    self.tableView.reloadData()//yeni bir veri geldiğini ve gösterileceğini söylüyoruz
+                }
+            }
+        }  //databasedeki post klasörüne kulaşabiliyorum şuan
         
     }
+    
+    
 
 }
